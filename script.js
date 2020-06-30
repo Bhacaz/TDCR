14a7fc7070ee105ae4065bec7ddb317b8f038fb8
Jean-Francis Bastien <bhacaz@gmail.com>
2020-06-30 08:49:36 -0400

    Change colors

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
 var map;
 
-function colorGenerator() {
-    const baseColor = [0, 128, 255];
-    let colors = [];
-
-    for (let i = 0; i <  3; i++) {
-        for (let j = 0; j < 3; j++) {
-            for (let k = 0; k < 3; k++) {
-                const newColor = [baseColor[i], baseColor[j], baseColor[k]];
-                if (newColor.toString() !== [0, 0, 0].toString() && newColor.toString() !== [255, 255, 255].toString() && newColor.toString() !== [128, 128, 128].toString()) {
-                    colors.push([baseColor[i], baseColor[j], baseColor[k]])
-                }
-            }
-        }
-    }
-    return colors;
-}
+const colors = ["#00755E", "#0095B7", "#FBE7B2", "#FF91A4", "#1560BD", "#FE4C40", "#FC80A5", "#00CC99", "#FD0E35", "#FFAE42", "#DEA681", "#B5B35C", "#ECEBBD", "#803790", "#E77200", "#8D90A1", "#FEBAAD", "#FF3399", "#01786F", "#FBE870", "#29AB87", "#EED9C4", "#8359A3", "#00CCCC", "#C88A65", "#AF593E", "#6B3FA0", "#837050", "#F7A38E", "#F7468A", "#CA3435", "#E30B5C", "#5FA777", "#4F69C6", "#DA3287", "#93CCEA", "#E97451", "#C62D42", "#7BA05B", "#DB5079", "#33CC99", "#926F5B", "#FF9980", "#BB3385", "#8E3179", "#C3CDE6", "#1AB385", "#C32148", "#93DFB8", "#6CDAE7", "#DA8A67", "#652DC1", "#E29CD2", "#FFA6C9", "#F091A9", "#FFFF99", "#FF681F", "#FE6F5E", "#63B76C", "#A50B5E", "#EDC9AF", "#B94E48", "#009DC4", "#9DE093", "#C9A0DC", "#FFB7D5", "#FF8833", "#FCD667", "#C5E17A", "#FDD7E4", "#FBAED2", "#AFE313", "#D27D46", "#76D7EA", "#ED0A3F", "#9E5B40", "#614051", "#FFB97B", "#02A4D3", "#6456B7", "#D6AEDD", "#0066CC", "#9999CC", "#FFCBA4", "#F653A6", "#0066FF", "#7A89B8", "#A9B2C3", "#87421F", "#F1E788", "#C154C1", "#FF7034"]
 
 function initMap() {
     // set up the map
@@ -29,19 +14,17 @@ function initMap() {
             style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
         }
     });
-    const colors = colorGenerator();
-
     const list = document.getElementById('trailList');
 
     map.data.loadGeoJson('TDCR.json', '', function(features) {
         console.log(features);
         features.forEach(function(feature, index) {
-            map.data.overrideStyle(feature, { strokeColor: 'rgb(' + colors[index][0] + ', ' + colors[index][1] + ', ' + colors[index][2] + ')'})
+            map.data.overrideStyle(feature, { strokeColor: colors[index]});
             const liElement = document.createElement('li');
             const dotElement = document.createElement('div');
             const spanName = document.createElement('span');
             dotElement.classList.add('dot');
-            dotElement.style.backgroundColor = 'rgb(' + colors[index][0] + ', ' + colors[index][1] + ', ' + colors[index][2] + ')';
+            dotElement.style.backgroundColor = colors[index];
             spanName.innerHTML = feature.j.name;
             liElement.appendChild(dotElement);
             liElement.appendChild(spanName);
