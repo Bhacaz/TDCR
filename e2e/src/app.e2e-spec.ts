b674e408807923784548a4a073c70bc382e673dc
Jean-Francis Bastien <bhacaz@gmail.com>
2020-07-24 12:03:57 -0400

    Rename ng project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to tdcr-ng!');
+    expect(page.getTitleText()).toEqual('Welcome to TDCR!');
   });
 
   afterEach(async () => {
